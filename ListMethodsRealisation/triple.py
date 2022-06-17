e0ff2d07d40e6af7a66b3e1cee6b2d2453f55d41
hovomuradyan <88653435+hovomuradyan@users.noreply.github.com>
2022-06-17 23:43:21 +0400

    Create triple.py

---
+++ ListMethodsRealisation/triple.py
@@ -0,0 +1,10 @@
+def triple(data):
+    data1 = []
+    for i in data:
+        data1.append(i * 3)
+    return data1
+
+
+# Testing example
+list1 = [100, 45, 125, 12, 124, 14, 45]
+print(triple(list1))
